2a5df138e619de1bdd3819f9b72d87b61af8b468
michaelb <michaelpb@gmail.com>
2017-04-25 15:05:42 -0700

    new features

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const adaptorPath = name => path.resolve(__dirname, `./lib/adaptors/${name}`)
 module.exports.ModuleBase = require('./lib/ModuleBase');
 module.exports.WindowManager = require('./lib/WindowManager');
 module.exports.launch = require('./lib/launcher').launch;
+module.exports.testutils = require('./lib/testutils');
 module.exports.adaptorPaths = {
     riotjs: adaptorPath('riotjs'),
 };
